bdcf0a8c35c17c293b6779740d202d7035272d3d
Guilherme França <guii_real@hotmail.com>
2020-01-23 23:32:06 -0300

    Todo

---
+++ frontend/src/pages/Todo/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import PageHeader from "../../components/PageHeader";
+
+export default class Todo extends Component {
+
+  render() {
+    return (
+      <>
+        <PageHeader name='Tarefas' small='Cadastro'/>
+      </>
+    );
+  }
+}
