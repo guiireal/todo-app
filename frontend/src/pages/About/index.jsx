09da24cdc5b88186e163cb718dbccd7bc4809889
Guilherme França <guii_real@hotmail.com>
2020-01-23 23:31:25 -0300

    About page

---
+++ frontend/src/pages/About/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import PageHeader from "../../components/PageHeader";
+
+const About = () => {
+  return (
+    <>
+      <PageHeader name='Sobre' small='Nós'/>
+      <h2>Nossa História</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis deleniti dicta eligendi esse et
+        hic id, ipsum minima natus numquam quas qui quis repellat saepe! Accusamus ad aut consectetur, dolorem enim, est
+        excepturi facilis harum hic neque quaerat sequi vel velit. Accusamus at aut blanditiis consequatur corporis
+        deleniti dolorum ea eligendi enim est ex explicabo in ipsum iste iusto minima natus nemo neque, numquam odio
+        pariatur perferendis quae quibusdam quidem quis sequi similique tempora totam, ullam velit vero vitae voluptas
+        voluptatibus. Adipisci animi consectetur culpa delectus ipsa maxime necessitatibus nihil, nobis nulla odio
+        perspiciatis porro quam quasi repellendus, sed.</p>
+      <h2>Missão e Visão</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at, beatae cumque deserunt distinctio eligendi
+        explicabo incidunt labore minus, natus nemo nesciunt numquam sapiente sequi sunt vel velit vitae voluptate.</p>
+      <h2>Imprensa</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, et ex facilis ipsam nam natus nobis
+        numquam obcaecati, quis repellat reprehenderit sed temporibus totam veniam voluptatum? Adipisci architecto
+        itaque minus?</p>
+    </>
+  );
+};
+
+export default About;
