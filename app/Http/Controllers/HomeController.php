55279e4064f5431abc970b52621fa303a98b3688
guiireal <guii_real@hotmail.com>
2024-06-02 18:10:21 -0300

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return inertia('Home', [
-            'canLogin' => Route::has('login'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
+        return inertia('Home');
     }
 }
